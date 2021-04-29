d3b11c0e03538fdaeca6b88d07cab065273254ea
ネコシャーク02 <82247089+nekoshark02@users.noreply.github.com>
2021-04-29 10:50:54 +0900

    カウントとvectorクラスについて

--- tukareta_count.cpp
+++ tukareta_count.cpp
@@ -6,13 +6,16 @@
 #include<vector>
 using namespace std;
 int main(void){
-  vector(int)v=[1,3,3,4,5,2,1];
+  vector<int>v={1,3,3,4,5,2,1};
   int cnt;
   cnt=count(v.begin(),v.end(),3);
   cout<<"Number 3 occurs"<<cnt<<"times."<<endl;
   
   return 0;
 }
+
+//vectorクラス　#include<vector>、vector<int>hoge={};でvectorクラスの使用宣言
+//vectorクラスは要素数が可変であり、かつ処理が高速である
 
 /*引用元　https://www.tutorialspoint.com/cpp_standard_library/cpp_algorithm_count.htm#:~:text=The%20C%2B%2B%20function%20std%3A%3Aalgorithm%3A%3Acount%20%
 28%29%20returns%20the%20number,range.%20This%20function%20uses%20operator%20%3D%3D%20for%20comparison.

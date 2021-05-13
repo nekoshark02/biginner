35410784f949d0d0b9032a7f8b20954a41426462
ネコシャーク02 <82247089+nekoshark02@users.noreply.github.com>
2021-05-13 22:40:09 +0900

    後々もう少し追記したい

---
+++ class.cpp
@@ -0,0 +1,25 @@
+//わかりやすいサイト見つけた。幸せ
+
+https://ja.wikibooks.org/wiki/C%2B%2B/%E5%88%9D%E5%BF%83%E8%80%85%E3%82%80%E3%81%91/%E3%82%AF%E3%83%A9%E3%82%B9
+
+/*なんでクラスなのかっていうことが詳しく書いてありました。カプセル化っていうのができるらしいです。カプセル化無しなら構造体でもいいらしいです
+では、作ってみる*/
+
+class menfis{
+  public;
+    int allmomo;
+    char dedede;
+}
+int main(){
+  menfis banana;
+  banana.allmomo;
+  cin>>allmomo\n;
+  
+  banana.dedede;
+  cin>>dedede\n;
+  
+  cout<<allmomo<<dedede\n;
+}
+
+/*まあ無茶苦茶なプログラムですが、動いていればいいです。動いていなければ直します*/
+  
